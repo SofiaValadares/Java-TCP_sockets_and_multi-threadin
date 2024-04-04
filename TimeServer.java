7e96d7e10be137a70d6ae42879c5a9f39ac1bae1
SofiaValadares <svc3@cesar.school>
2024-04-04 11:11:15 +0100

    codigo professor

---
+++ TimeServer.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+ 
+/**
+ * This program demonstrates a simple TCP/IP socket server.
+ *
+ * @author www.codejava.net
+ */
+public class TimeServer {
+ 
+    public static void main(String[] args) {
+        if (args.length < 1) return;
+ 
+        int port = Integer.parseInt(args[0]);
+ 
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
+            System.out.println("Server is listening on port " + port);
+ 
+            while (true) {
+                Socket socket = serverSocket.accept();
+ 
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+                String time = reader.readLine();
+
+                System.out.println("New client connected: "+ time);
+ 
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+ 
+                writer.println(new Date().toString());
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
